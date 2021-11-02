a54f8c25e4d346fabae28decc7c9edd0397a182b
Wisnu Prasojo Widianto <wisnuprsj@gmail.com>
2021-11-02 13:43:52 +0700

    add configuration for task

---
+++ src/main/java/guru/springframework/sfgjms/config/TaskConfig.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgjms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by wp on 2021-11-02
+ */
+@EnableScheduling
+@EnableAsync
+@Configuration
+public class TaskConfig {
+
+    @Bean
+    TaskExecutor taskExecutor() {
+        return new SimpleAsyncTaskExecutor();
+    }
+
+}
